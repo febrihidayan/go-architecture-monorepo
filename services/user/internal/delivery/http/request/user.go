a24c8415af50e2367ba0065bd915c34dacaae052
Febri Hidayan <febrihidayan20@gmail.com>
2023-12-22 22:39:52 +0700

    adjust validate email

--- services/user/internal/delivery/http/request/user.go
+++ services/user/internal/delivery/http/request/user.go
@@ -4,15 +4,15 @@ import "github.com/febrihidayan/go-architecture-monorepo/pkg/common"
 
 type UserCreateRequest struct {
 	Name     string `json:"name" validate:"required|min:3"`
-	Email    string `json:"email" validate:"required|min:3"`
+	Email    string `json:"email" validate:"required|email|min:3"`
 	Password string `json:"password" validate:"required|min:6"`
 }
 
 type UserUpdateRequest struct {
 	ID       *common.ID `param:"id" validate:"required"`
 	Name     string     `json:"name" validate:"required|min:3"`
-	Email    string     `json:"email" validate:"required|min:3"`
-	Password string     `json:"password" validate:"required|min:6"`
+	Email    string     `json:"email" validate:"required|email|min:3"`
+	Password string     `json:"password" validate:"nullable|min:6"`
 }
 
 type UserQueryParams struct {
